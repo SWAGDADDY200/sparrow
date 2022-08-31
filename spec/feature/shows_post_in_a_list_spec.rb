626e6d15bed8de55c2db1db3b4c38f91adce3a42
SWAGDADDY200 <christophertomlinson06@gmail.com>
2022-08-31 13:57:45 -0500

    Tests completed

--- spec/feature/shows_post_in_a_list_spec.rb
+++ spec/feature/shows_post_in_a_list_spec.rb
@@ -1,10 +1,11 @@
 require "rails_helper"
 
-RSpec.feature 'Posts', type: :feature do
+RSpec.feature 'Posts', type: :feature, js: true do
   scenario 'Visiting the home when there posts created shows the post in a list' do
+    post = Post.create(author: "James Matthews", body: "I love oranges")
     visit '/'
-    # expect(page.body).to match_array(@posts)
-    let(:post) { Post.last }
-    expect(page.body).to have_css(@post.id, text: "blah blah blah")
+    expect(page.body).to have_text(post.body.to_plain_text)
+    expect(page.body).to have_text(post.author)
+
   end
 end
